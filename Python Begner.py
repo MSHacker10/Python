160c8cb0cccd3a1c1e07be747895ed4332dba74b
Muhammad Mohsin <107849236+MSHacker10@users.noreply.github.com>
2023-09-14 22:04:07 +0500

    Add files via upload

---
+++ Python Begner.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1 style="font-size:3rem;color:blue;"
+
+# <h1 style="font-size:3rem;color:orange;"
+
+# <h1 style=font-size:3rem;color:red;>Eesha Alam<h1>
+
+# <h1 style="font-size:3rem;color:blue;">Jupyter Notebook</h2>
+
+# # Python for Beginners
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# ## Insert a variable into a string
+
+# In[3]:
+
+
+person = input("What's your name?")
+message = "Hello, {}!".format(person)
+print(message)
+
+
+# ## Multiline Strings and String Length
+
+# In[4]:
+
+
+story = """Once upon a time
+there live a bear family
+who ate delicious pizza"""
+print(story)
+print(len(story))
+
+
+# ## Maths Operations
+
+# In[5]:
+
+
+x = 7
+y = 8 
+z = (x * 3) / y
+m = (x * 3) % y #modulo returns the remainder
+print(z)
+print(m)
+
+
+# ## Lists (arrays)
+
+# In[6]:
+
+
+friends = ['Ania' , 'Sana' , 'Hina' , 'Rachel' , 'Ali' , 'Ahmad']
+print(friends[2])
+
+
+# In[ ]:
+
+
+
+
